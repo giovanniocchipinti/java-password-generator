7a0b429a0427cf694aa1a59518f99392e7456403
giovanniocchipinti <giovanniocchipinti2699@gmail.com>
2024-11-12 22:16:30 +0100

    init: project init, created first scaffholding, added package and defined variables

---
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -0,0 +1,34 @@
+package org.lessons.java.security;
+import java.util.Scanner;
+
+public class PasswordGenerator {
+    public static void main(String[] args) {
+
+
+    
+
+
+        System.out.println("Inserisci i dati richiesti per generare una password:");
+
+        System.out.println("Inserisci il tuo nome e premi invio");
+        String userName = "";
+
+        System.out.println("Inserisci il tuo cognome e premi invio:");
+        String userSurname = "";
+
+        System.out.println("Inserisci il tuo colore preferito e premi invio:");
+        String userFavCol = "";
+
+        System.out.println("Inserisci il tuo giorno di nascita e premi invio");
+        int userBirthDay = 0;
+
+        System.out.println("Inserisci il tuo mese di nascita in formato numero e premi invio");
+        int userBirthMonth = 0;
+
+        System.out.println("Inserisci il tuo anno di nascita e premi invio");
+        int userBirthYear = 0; 
+
+ 
+
+    }
+}
